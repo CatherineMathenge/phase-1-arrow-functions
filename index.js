b2cb4c5daf7c09e4bc3d93cfa72e4a861a279fe3
Catherine <catherine.mathenge@strathmore.edu>
2023-06-25 21:50:30 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide= (num1, num2) => {
+    console.log (num1 / num2);
+    return 20;
+}
+doMath(2000,100)
+
+const square = (num) => {
+        return num * num;
+}
+ doMath(2,2)
+  
+const add = (a,b) => a + b;
+  add(3,4); //=> 7
+
+
+
+  
+
+  
